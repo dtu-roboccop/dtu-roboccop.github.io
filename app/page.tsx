bc3d46d16f460665b486bb5e5438ce0e3d41a003
aka-lowe <93315804+aka-lowe@users.noreply.github.com>
2026-02-11 09:40:24 +0100

    rm sponsors page

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Page() {
         <Events />
         <Projects />
         <Team />
-        <Sponsors />
+        {/* <Sponsors /> */}
         <Contact />
       </main>
       <Footer />
